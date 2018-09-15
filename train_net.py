be87f64de9e1a3b185d20638310b946de2c5256e
fedshyvana <mlu21@jhu.edu>
2018-09-15 09:43:59 -0400

    modified

--- train_net.py
+++ train_net.py
@@ -117,7 +117,7 @@ def train_loop(model, loaders, optimizer, epochs=10, filename=None):
 				acc = check_accuracy(loader_val, model, train=True)
 				print()
 
-    acc = check_accuracy(loader_val, model, train=False, filename)
+	acc = check_accuracy(loader_val, model, train=False, filename)
 	return acc
 
 def train_network(ssh = True):
@@ -134,9 +134,9 @@ def train_network(ssh = True):
 											   transforms.RandomVerticalFlip(),
 											   transforms.RandomHorizontalFlip(),
 											   transforms.ToTensor(),
-										       transforms.Normalize(mean=[0.485, 0.456, 0.406],
+											   transforms.Normalize(mean=[0.485, 0.456, 0.406],
 															std=[0.229, 0.224, 0.225])
-										       ])
+											   ])
 
 	transformation_val = transforms.Compose([transforms.Resize([224, 224]),
 											 transforms.ToTensor(),
