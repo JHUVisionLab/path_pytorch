baed8a8bfa238086d6c6fbb8f6b0e07ac0f9ad52
fedshyvana <mlu21@jhu.edu>
2018-09-15 22:21:21 -0400

    modified

--- resnet.py
+++ resnet.py
@@ -152,7 +152,7 @@ class ResNet(nn.Module):
         x = x.view(x.size(0), -1)
         x = self.fc1(x)
         x = self.dropout(x)
-        x = F.reLu(x)
+        x = F.relu(x)
         x = self.fc2(x)
 
         return x
