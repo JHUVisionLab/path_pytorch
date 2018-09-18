94e5195dedea76c959ca578eb614c9c44ca8dfe9
fedshyvana <mlu21@jhu.edu>
2018-09-17 21:28:01 -0400

    tiling

--- PathologyDataset.py
+++ PathologyDataset.py
@@ -21,98 +21,98 @@ import torch.nn.functional as F  # useful stateless functions
 
 import nets 
 class PathologyDataset(Dataset):
-    """Pathology dataset"""
-    def __init__(self, root_dir, csv_file = 'microscopy_ground_truth.csv', transform=transforms.ToTensor(), shuffle = False):
-        """
-        Args:
-            csv_file (string): Path to the csv file with annotations.
-            root_dir (string): Directory with all the images.
-            transform (callable, optional): Optional transform to be applied on a sample
-            shuffle (boolean): Whether to shuffle
-        """
-        data = pd.read_csv(os.path.join(root_dir, "microscopy_ground_truth.csv"), header = None).values
-        
-        #shuffle data
-        if shuffle:
-            np.random.shuffle(data)
-        
-        img_ids = data[:,0]
-        img_classes = data[:,1]
-        img_labels = np.zeros((len(img_classes),), dtype=np.int32)
-        class_to_label = {'Normal':0, 'Benign':1, "InSitu":2, 'Invasive':3}
-        print('Class to label dictionary map: ')
-        print(class_to_label)     
+	"""Pathology dataset"""
+	def __init__(self, root_dir, csv_file = 'microscopy_ground_truth.csv', transform=transforms.ToTensor(), shuffle = False):
+		"""
+		Args:
+			csv_file (string): Path to the csv file with annotations.
+			root_dir (string): Directory with all the images.
+			transform (callable, optional): Optional transform to be applied on a sample
+			shuffle (boolean): Whether to shuffle
+		"""
+		data = pd.read_csv(os.path.join(root_dir, "microscopy_ground_truth.csv"), header = None).values
+		
+		#shuffle data
+		if shuffle:
+			np.random.shuffle(data)
+		
+		img_ids = data[:,0]
+		img_classes = data[:,1]
+		img_labels = np.zeros((len(img_classes),), dtype=np.int32)
+		class_to_label = {'Normal':0, 'Benign':1, "InSitu":2, 'Invasive':3}
+		print('Class to label dictionary map: ')
+		print(class_to_label)     
 
-        for i in range(len(img_ids)):
-            img_ids[i] = str(img_classes[i])+'/'+img_ids[i]
-            #map from string class names to integer labels
-            img_labels[i] = class_to_label[str(img_classes[i])]
+		for i in range(len(img_ids)):
+			img_ids[i] = str(img_classes[i])+'/'+img_ids[i]
+			#map from string class names to integer labels
+			img_labels[i] = class_to_label[str(img_classes[i])]
 
-        self.img_ids = img_ids
-        self.img_labels = img_labels
-        self.root_dir = root_dir
-        self.transform = transform
+		self.img_ids = img_ids
+		self.img_labels = img_labels
+		self.root_dir = root_dir
+		self.transform = transform
 
-    def __len__(self):
-        return len(self.img_ids)
+	def __len__(self):
+		return len(self.img_ids)
 
-    def __getitem__(self, idx):
-        img_name = os.path.join(self.root_dir,
-                                self.img_ids[idx])
-        
-        img = Image.open(img_name, mode='r')
-        label = self.img_labels[idx]
-        
-        if self.transform:
-            img = self.transform(img)
+	def __getitem__(self, idx):
+		img_name = os.path.join(self.root_dir,
+								self.img_ids[idx])
+		
+		img = Image.open(img_name, mode='r')
+		label = self.img_labels[idx]
+		
+		if self.transform:
+			img = self.transform(img)
 
-        return img, label
+		return img, label
   
   class PathologyDataset_Tiling(Dataset):
-    """Pathology dataset"""
-    def __init__(self, root_dir, csv_file = 'microscopy_ground_truth.csv', transform=transforms.ToTensor(), shuffle = False, tiling = False):
-        """
-        Args:
-            csv_file (string): Path to the csv file with annotations.
-            root_dir (string): Directory with all the images.
-            transform (callable, optional): Optional transform to be applied on a sample
-            shuffle (boolean): Whether to shuffle
-        """
-        data = pd.read_csv(os.path.join(root_dir, "microscopy_ground_truth.csv"), header = None).values
-        
-        #shuffle data
-        if shuffle:
-            np.random.shuffle(data)
-        
-        img_ids = data[:,0]
-        img_classes = data[:,1]
-        img_labels = np.zeros((len(img_classes),), dtype=np.int32)
-        class_to_label = {'Normal':0, 'Benign':1, "InSitu":2, 'Invasive':3}
-        print('Class to label dictionary map: ')
-        print(class_to_label)     
+	"""Pathology dataset"""
+	def __init__(self, root_dir, csv_file = 'microscopy_ground_truth.csv', transform=transforms.ToTensor(), shuffle = False, tiling = False):
+		"""
+		Args:
+			csv_file (string): Path to the csv file with annotations.
+			root_dir (string): Directory with all the images.
+			transform (callable, optional): Optional transform to be applied on a sample
+			shuffle (boolean): Whether to shuffle
+		"""
+		data = pd.read_csv(os.path.join(root_dir, "microscopy_ground_truth.csv"), header = None).values
+		
+		#shuffle data
+		if shuffle:
+			np.random.shuffle(data)
+		
+		img_ids = data[:,0]
+		img_classes = data[:,1]
+		img_labels = np.zeros((len(img_classes),), dtype=np.int32)
+		class_to_label = {'Normal':0, 'Benign':1, "InSitu":2, 'Invasive':3}
+		print('Class to label dictionary map: ')
+		print(class_to_label)     
 
-        for i in range(len(img_ids)):
-            img_ids[i] = str(img_classes[i])+'/'+img_ids[i]
-            #map from string class names to integer labels
-            img_labels[i] = class_to_label[str(img_classes[i])]
+		for i in range(len(img_ids)):
+			img_ids[i] = str(img_classes[i])+'/'+img_ids[i]
+			#map from string class names to integer labels
+			img_labels[i] = class_to_label[str(img_classes[i])]
 
-        self.img_ids = img_ids
-        self.img_labels = img_labels
-        self.root_dir = root_dir
-        self.transform = transform
+		self.img_ids = img_ids
+		self.img_labels = img_labels
+		self.root_dir = root_dir
+		self.transform = transform
 
-    def __len__(self):
-        return len(self.img_ids)
+	def __len__(self):
+		return len(self.img_ids)
 
-    def __getitem__(self, idx):
-        img_name = os.path.join(self.root_dir,
-                                self.img_ids[idx])
-        
-        img = Image.open(img_name, mode='r')
-        label = self.img_labels[idx]
-        
-        if self.transform:
-            img = self.transform(img)
+	def __getitem__(self, idx):
+		img_name = os.path.join(self.root_dir,
+								self.img_ids[idx])
+		
+		img = Image.open(img_name, mode='r')
+		label = self.img_labels[idx]
+		
+		if self.transform:
+			img = self.transform(img)
 
-        return img, label  
+		return img, label  
 
