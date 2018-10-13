b79179be3b4fbfb6d4e21578ac68f02edf7e2c59
fedshyvana <mlu21@jhu.edu>
2018-10-13 16:19:18 -0400

    modified

--- resnet_helper.py
+++ resnet_helper.py
@@ -75,7 +75,9 @@ def tile_images(images, res):
 	del images
 	counter=0
 	for im in im_list:
-		tiles = [eval(f_dict[str(i)]) for i in res]
+		for i in res:
+		# tiles = [eval(f_dict[str(i)]) for i in res]
+			tiles = eval(f_dict[str(i)])
 		im_list[counter] = torch.cat(tiles,0)
 		counter+=1
   
