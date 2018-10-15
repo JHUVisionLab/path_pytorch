473a6d2c72c62b760f7d8c84480ee9826608b13c
fedshyvana <mlu21@jhu.edu>
2018-10-15 13:21:36 -0400

    updated 10/15

--- cross_validation.py
+++ cross_validation.py
@@ -45,31 +45,45 @@ def k_folds(n_splits = 10, samples = 400):
         train_idx = np.setdiff1d(indices, test_idx)
         yield train_idx, test_idx
 
-def k_folds_2(n_splits = 10, samples = 400, num_classes = 4):
+def k_folds_2(n_splits = 10, samples = 400, num_classes = 4, monte_carlo = False):
     indices = np.arange(samples).astype(int)
-    for test_idx in get_indices_2(n_splits, samples, num_classes):
+    for test_idx in get_indices_2(n_splits, samples, num_classes, monte_carlo = monte_carlo):
         train_idx = np.setdiff1d(indices, test_idx)
         yield train_idx, test_idx
 
-def get_indices_2(n_splits = 10, samples = 400, num_classes = 4):
-    subfold_sizes = int(samples/n_splits/num_classes) #400/10/4 = 10 
-    samples_per_class = int(samples/num_classes) #400/4 = 100
+def get_indices_2(n_splits = 10, samples = 400, num_classes = 4, monte_carlo = False):
+    # using n_splits = 10 as example
+    if monte_carlo:
+        subfold_sizes = int(samples*0.1/num_classes) #400/10/4 = 10 
+    else:
+        subfold_sizes = int(samples/n)
+    
+    samples_per_class = int(samples/n_splits/num_classes) #400/4 = 100
     
     indices = np.arange(samples).astype(int)
     current = 0
     
     for i in range(n_splits):
-        start = current #0, 10, 20, 30, 40, 50, 60, 70, 80, 90
-        stop =  current + subfold_sizes #10, 20, 30, 40, 50, 60, 70, 80, 90, 100
-        current = stop 
         idx = np.empty((0,)).astype(int)
-        for c in range(num_classes): #0, 1, 2, 3
-            idx = np.append(idx, indices[int(start+c*samples_per_class):int(stop+c*samples_per_class)])
-            #0:10, 100:110, 200:210, 300:310
-            #10:20, 110:120, 210:220, 310:320
-            #...
-            #90:100, 190:200, 290:300, 390:400
-        yield idx
+
+        if monte_carlo:
+            for c in range(num_classes):
+                idx = np.append(idx, np.random.choice(indices[int(c*samples_per_class):int((c+1)*samples_per_class)], subfold_sizes, replace = False))
+
+            yield idx
+        
+        else: 
+            start = current #0, 10, 20, 30, 40, 50, 60, 70, 80, 90
+            stop =  current + subfold_sizes #10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+            current = stop 
+            for c in range(num_classes): #0, 1, 2, 3
+                idx = np.append(idx, indices[int(start+c*samples_per_class):int(stop+c*samples_per_class)])
+                #0:10, 100:110, 200:210, 300:310
+                #10:20, 110:120, 210:220, 310:320
+                #...
+                #90:100, 190:200, 290:300, 390:400
+            yield idx
+
 
 def test_kfold(k = 10, samples = 400):
     for train_idx, test_idx in k_folds(n_splits = 10, samples = samples):
@@ -81,11 +95,12 @@ def test_kfold(k = 10, samples = 400):
         s = list(train_idx.intersection(test_idx))
         assert s == []
 
-def test_kfold_2(k = 10, samples = 400, num_classes = 4):
-    for train_idx, test_idx in k_folds_2(n_splits = 10, samples = samples, num_classes=num_classes):
+def test_kfold_2(k = 15, samples = 400, num_classes = 4, monte_carlo=True):
+    for train_idx, test_idx in k_folds_2(n_splits = 10, samples = samples, num_classes=num_classes, monte_carlo=monte_carlo):
         print(test_idx)
-        assert np.unique(train_idx).size == samples/k*(k-1)
-        assert np.unique(test_idx).size == samples/k
+        if not monte_carlo:
+            assert np.unique(train_idx).size == samples/k*(k-1)
+            assert np.unique(test_idx).size == samples/k
         train_idx = set(train_idx)
         test_idx = set(test_idx)
         s = list(train_idx.intersection(test_idx))
